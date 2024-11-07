d3cd065cbe9da262a7bcfe017d546319bf53449d
Julianeves01 <julianvs2007@gmail.com>
2024-11-07 15:09:30 -0300

    Feat: create method get

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -16,7 +16,7 @@ usuariosRoutes.get("/", (req, res) => {
     return res.status(200).json({
         message:
             usuarios.length == 0
-            ? "Não há usuários cadastrados"
+            ? "Não há existencia de usuarios cadastrados!"
             : `Total de usuários: ${usuarios.length}`,
         usuarios,
     });
