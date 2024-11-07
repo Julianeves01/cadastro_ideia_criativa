6c26780d9459c5c1cf6ab3228109ebca2d7efe5c
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-11-07 20:12:21 -0300

    feat: create user class

---
+++ src/routes/models/User.js
@@ -0,0 +1,15 @@
+class User {
+    constructor( titulo, categorias, prioridade) {
+        this.id = this.generateId();
+        this.titulo = this.titulo;
+        this.categorias = categorias;
+        this.prioridade = prioridade;
+        this.feedback = [];
+    }
+
+    generateId() {
+        return Math.floor(Math.random() *999999) + 1;
+    }
+}
+
+export default User;
